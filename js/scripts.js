bdc874c2047345e301d3fe09bf656a0805ad6c17
Franklin Waggoner <fcw1987@mac.com>
2019-09-26 09:32:42 -0700

    adds correct js for showing and hiding the results when numbers are selected

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,31 @@
 // Back-End logic
 $(document).ready(function() {
+
   $("#submitForm").click(function(){
       var firstName = $("input#firstName").val();
       var email = $("input#emailAddress").val();
       var result = parseInt($("select#result").val());
-
+      $("#javaScript").hide();
+      $("#cSharp").hide();
+      $("#go").hide();
+      $("#python").hide();
+      $("#swifthide").hide();
 
 
       if (result == 1 || result == 2) {
-        alert("Python");
+        $("#javaScript").show();
       }
       else if (result == 3 || result == 4) {
-        alert("cSharp");
+        $("#cSharp").show();
       }
       else if (result == 5 || result == 6) {
-        alert("Go");
+        $("#go").show();
       }
       else if (result == 7 || result == 8) {
-        alert("Swift");
+        $("#python").show();
       }
       else if (result == 9 || result == 10) {
-        alert("JavaScript");
+        $("#swift").show();
       }
       else {
         alert("Error has been detected");
