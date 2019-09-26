d020343a5bc907b839ba837b596bd87fe459b7f4
Franklin Waggoner <fcw1987@mac.com>
2019-09-26 09:12:57 -0700

    adds working js for alerting the name of a language

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,24 @@ $(document).ready(function() {
 
 
 
-if (result == 1 || result == 2) {
-  alert(result);
-}
-if else (result == 3 || result == 4) {
-  alert("wrong");
-}
-else {
-  alert("nope");
-}
+      if (result == 1 || result == 2) {
+        alert("Python");
+      }
+      else if (result == 3 || result == 4) {
+        alert("cSharp");
+      }
+      else if (result == 5 || result == 6) {
+        alert("Go");
+      }
+      else if (result == 7 || result == 8) {
+        alert("Swift");
+      }
+      else if (result == 9 || result == 10) {
+        alert("JavaScript");
+      }
+      else {
+        alert("Error has been detected");
+      }
 
     event.preventDefault();
 
